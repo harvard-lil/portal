851fbabfeecc300c63cb0ed633394284685a483c
Greg Leppert <greg@leppert.me>
2023-04-01 16:08:44 -0400

    Fix tests

--- Portal.test.js
+++ Portal.test.js
@@ -1,6 +1,7 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 import * as http from 'http'
+import { Transform } from 'node:stream'
 
 import { createServer } from './Portal.js'
 
@@ -10,8 +11,8 @@ const PROXY_HOST = '127.0.0.1'
 const SERVER_PORT = 3000
 const SERVER_HOST = '127.0.0.1'
 
-const runInProxy = async (cb) => {
-  const proxy = createServer()
+const runInProxy = async (options, cb) => {
+  const proxy = createServer(options)
   await new Promise(resolve => proxy.listen(PROXY_PORT, PROXY_HOST, resolve))
   await new Promise(resolve => cb(resolve, proxy))
   proxy.on('error', () => {}) // silence any socket closed errors before closeAllConnections()
@@ -24,7 +25,8 @@ const requestOptions = {
   host: PROXY_HOST,
   path: `http://${SERVER_HOST}:${SERVER_PORT}`,
   headers: {
-    Host: SERVER_HOST
+    host: SERVER_HOST,
+    echo: 'Hello world'
   }
 }
 
@@ -32,9 +34,9 @@ await test('Portal', async (t) => {
   /*
    * SETUP
    */
-  const server = http.createServer((_req, res) => {
+  const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello world')
+    res.end(req.headers.echo)
   }).listen(SERVER_PORT)
 
   /*
@@ -45,22 +47,46 @@ await test('Portal', async (t) => {
   })
 
   await t.test('\'request\' event returns instance of http.IncomingMessage', async () => {
-    await runInProxy((resolve, proxy) => {
+    await runInProxy({}, (resolve, proxy) => {
       http.get(requestOptions)
       proxy.on('request', (request) => {
-        assert(request.constructor, http.IncomingRequest)
+        assert.equal(request.constructor, http.IncomingMessage)
         resolve()
       })
     })
   })
 
   await t.test('\'response\' event returns instances of http.IncomingMessage', async () => {
-    await runInProxy((resolve, proxy) => {
+    await runInProxy({}, (resolve, proxy) => {
       http.get(requestOptions)
       proxy.on('response', (response, request) => {
-        assert(response.constructor, http.IncomingRequest)
-        assert(request.constructor, http.IncomingRequest)
+        assert.equal(response.constructor, http.IncomingMessage)
+        assert.equal(request.constructor, http.IncomingMessage)
         resolve()
+      })
+    })
+  })
+
+  await t.test('requestTransformer modifies the request', async () => {
+    const echo = requestOptions.headers.echo
+    const newEcho = 'Hello everybody!'
+
+    const options = {
+      requestTransformer: () => new Transform({
+        transform: (chunk, _encoding, callback) => {
+          callback(null, Buffer.from(chunk.toString().replace(echo, newEcho)))
+        }
+      }),
+    }
+
+    await runInProxy(options, (resolve, proxy) => {
+      http.get(requestOptions)
+      proxy.on('response', (response, request) => {
+        assert.equal(request.headers.echo, echo)
+        response.on('data', data => {
+          assert.equal(data.toString(), newEcho)
+          resolve()
+        })
       })
     })
   })
