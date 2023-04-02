1f0151494734bdfbbf74e4a7d4e14fb6dfcf4d60
Greg Leppert <greg@leppert.me>
2023-04-01 21:00:36 -0400

    Fix tests

--- Portal.test.js
+++ Portal.test.js
@@ -1,7 +1,7 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
-import * as http from 'http'
 import { Transform } from 'node:stream'
+import * as http from 'http'
 
 import { createServer } from './Portal.js'
 
@@ -13,11 +13,18 @@ const SERVER_HOST = '127.0.0.1'
 
 const runInProxy = async (options, cb) => {
   const proxy = createServer(options)
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(req.headers.echo)
+  })
+
   await new Promise(resolve => proxy.listen(PROXY_PORT, PROXY_HOST, resolve))
+  await new Promise(resolve => server.listen(SERVER_PORT, SERVER_HOST, resolve))
+
   await new Promise(resolve => cb(resolve, proxy))
-  proxy.on('error', () => {}) // silence any socket closed errors before closeAllConnections()
-  proxy.closeAllConnections()
+
   await new Promise(resolve => proxy.close(resolve))
+  await new Promise(resolve => server.close(resolve))
 }
 
 const requestOptions = {
@@ -26,101 +33,94 @@ const requestOptions = {
   path: `http://${SERVER_HOST}:${SERVER_PORT}`,
   headers: {
     host: SERVER_HOST,
+    connection: 'close',
     echo: 'Hello world'
   }
 }
 
-await test('Portal', async (t) => {
-  /*
-   * SETUP
-   */
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(req.headers.echo)
-  }).listen(SERVER_PORT)
+test('createServer returns an instance of http.Server', async () => {
+  assert.equal(createServer().constructor, http.Server)
+})
 
-  /*
-   * TESTS
-   */
-  t.test('createServer returns an instance of http.Server', async () => {
-    assert.equal(createServer().constructor, http.Server)
+await test('\'request\' event returns instance of http.IncomingMessage', async () => {
+  await runInProxy({}, (resolve, proxy) => {
+    proxy.on('request', (request) => {
+      assert.equal(request.constructor, http.IncomingMessage)
+    })
+    http
+      .get(requestOptions)
+      .on('close', resolve)
   })
+})
 
-  await t.test('\'request\' event returns instance of http.IncomingMessage', async () => {
-    await runInProxy({}, (resolve, proxy) => {
-      proxy.on('request', (request) => {
-        assert.equal(request.constructor, http.IncomingMessage)
+await test('\'response\' event returns instances of http.IncomingMessage', async () => {
+  console.log('test start')
+  await runInProxy({}, (resolve, proxy) => {
+    proxy.on('response', (response, request) => {
+      assert.equal(response.constructor, http.IncomingMessage)
+      assert.equal(request.constructor, http.IncomingMessage)
+    })
+    http
+      .get(requestOptions)
+      .on('close', () => {
+        console.log('close')
         resolve()
       })
-      http.get(requestOptions)
-    })
-  })
-
-  await t.test('\'response\' event returns instances of http.IncomingMessage', async () => {
-    await runInProxy({}, (resolve, proxy) => {
-      proxy.on('response', (response, request) => {
-        assert.equal(response.constructor, http.IncomingMessage)
-        assert.equal(request.constructor, http.IncomingMessage)
-        resolve()
+      .on('error', (e) => {
+        console.log('error')
       })
-      http.get(requestOptions)
-    })
   })
+})
 
-  await t.test('requestTransformer modifies the request', async () => {
-    const echo = requestOptions.headers.echo
-    const newEcho = 'hELLO WORLD'
+await test('requestTransformer modifies the request', async () => {
+  const echo = requestOptions.headers.echo
+  const newEcho = 'hELLO WORLD'
 
-    const options = {
-      requestTransformer: () => new Transform({
-        transform: (chunk, _encoding, callback) => {
-          callback(null, Buffer.from(chunk.toString().replace(echo, newEcho)))
-        }
-      }),
-    }
+  const options = {
+    requestTransformer: () => new Transform({
+      transform: (chunk, _encoding, callback) => {
+        callback(null, Buffer.from(chunk.toString().replace(echo, newEcho)))
+      }
+    }),
+  }
 
-    await runInProxy(options, (resolve, proxy) => {
-      proxy.on('response', (response, request) => {
-        assert.equal(request.headers.echo, echo)
+  await runInProxy(options, (resolve, proxy) => {
+    proxy.on('response', (response, request) => {
+      assert.equal(request.headers.echo, echo)
+      response.on('data', data => {
+        assert.equal(data.toString(), newEcho)
+      })
+    })
+    http
+      .get(requestOptions)
+      .on('close', resolve)
+  })
+})
+
+await test('responseTransformer modifies the response', async () => {
+  const echo = requestOptions.headers.echo
+  const newEcho = 'hELLO WORLD'
+
+  const options = {
+    responseTransformer: () => new Transform({
+      transform: (chunk, _encoding, callback) => {
+        callback(null, Buffer.from(chunk.toString().replace(echo, newEcho)))
+      }
+    }),
+  }
+
+  await runInProxy(options, (resolve, proxy) => {
+    proxy.on('response', (_response, request) => {
+      assert.equal(request.headers.echo, echo)
+    })
+
+    http
+      .get(requestOptions)
+      .on('close', resolve)
+      .on('response', response => {
         response.on('data', data => {
           assert.equal(data.toString(), newEcho)
-          resolve()
         })
       })
-      http.get(requestOptions)
-    })
   })
-
-  await t.test('responseTransformer modifies the response', async () => {
-    const echo = requestOptions.headers.echo
-    const newEcho = 'hELLO WORLD'
-
-    const options = {
-      responseTransformer: () => new Transform({
-        transform: (chunk, _encoding, callback) => {
-          callback(null, Buffer.from(chunk.toString().replace(echo, newEcho)))
-        }
-      }),
-    }
-
-    await runInProxy(options, (resolve, proxy) => {
-      proxy.on('response', (_response, request) => {
-        assert.equal(request.headers.echo, echo)
-      })
-
-      http
-        .get(requestOptions)
-        .on('response', response => {
-          response.on('data', data => {
-            assert.equal(data.toString(), newEcho)
-            resolve()
-          })
-        })
-    })
-  })
-
-  /*
-   * TEARDOWN
-   */
-  server.close()
 })
